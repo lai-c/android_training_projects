901ad916e5d932d3e82752a472412973cc96b573
lai-c <lai-c@klab.com>
2013-04-02 14:51:34 +0900

    init bgmdemo ant release

---
+++ BgmDemo/gen/R.java.d
@@ -0,0 +1,17 @@
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/gen/com/example/bgmdemo/R.java \
+ : /Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/drawable-hdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/drawable-mdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/drawable-xhdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/drawable-xxhdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/layout/activity_fullscreen.xml \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/values/attrs.xml \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/values/colors.xml \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/values/strings.xml \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/values/styles.xml \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/values-v11/styles.xml \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/res/values-v14/styles.xml \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/bin/res/drawable-xhdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/bin/res/drawable-xxhdpi/ic_launcher.png \
+/Users/lai-c/laiconglin/android_training_projects/BgmDemo/bin/AndroidManifest.xml \
